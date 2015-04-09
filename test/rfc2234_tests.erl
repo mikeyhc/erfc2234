845f5edeaf8ad46c3b3d3ee7ee8d4116c64af4af
mikeyhc <mikeyhc@atmosia.net>
2015-04-09 17:31:58 +1200

    added more tests

--- test/rfc2234_tests.erl
+++ test/rfc2234_tests.erl
@@ -17,6 +17,18 @@ test_helper_(M, F, L1, L2, L3) ->
 alphaset() -> lists:append(lists:seq(65, 90),   % A-Z
                            lists:seq(97, 122)). % a-z
 
+%% all 8 bit chars
+list_1() -> lists:map(fun(X) -> <<X>> end, lists:seq(0, 255)).
+
+%% all 8 bit strings of length 2
+list_2() -> lists:concat(lists:map(fun(X) ->
+                lists:map(fun(Y) -> <<X,Y>> end, lists:seq(0, 255))
+                end, lists:seq(0,255))).
+
+%% all combinations of 8 bit strings of length 2
+
+
+%% the list of all common types which are not binary
 nonbinary_typeset() -> [ [], a, "", 0.0, fun() -> io:format("oh no~n") end,
                          {a, tuple}].
 
@@ -69,3 +81,39 @@ cr_test_() ->
                       lists:delete(13, lists:seq(0, 256))),
     test_helper_(rfc2234, cr, [{<<"\rab">>, 13, <<"ab">>}], Fails,
                  nonbinary_typeset()).
+
+%% check the correct functioning of the lf/1 function,
+%% this includes:
+%%  - make sure that if the first element in the binary is \n that it is
+%%    returned along with the remaining binary
+%%  - that if it is not \n that a parse error of the type expected is thrown
+%%  - that if a non-binary is passed in that a badarg error is thrown
+lf_test_() ->
+    Fails = lists:map(fun(X) -> <<X>> end,
+                      lists:delete(10, lists:seq(0, 256))),
+    test_helper_(rfc2234, lf, [{<<"\nab">>, 10, <<"ab">>}], Fails,
+                 nonbinary_typeset()).
+
+%% check the correct functioning of the crlf/1 function,
+%% this includes:
+%%  - make sure that if the prefix of the binary is \r\n, and that this is
+%%    returned along with the remaining binary
+%%  - that if it is not \r\n a parse error of the type expected is thrown
+%%  - that if a non-binary is passed in that a badarg error is thrown
+crlf_test_() ->
+    Fails = lists:delete(<<"\r\n">>, list_2()),
+    test_helper_(rfc2234, crlf, [{<<"\r\n-">>, <<"\r\n">>, <<"-">>}],
+                 Fails, nonbinary_typeset()).
+
+%% check the correct functioning of the crl/1 function,
+%% this includes:
+%%  - make sure that if the first element of the binary is a control
+%%    character that it is returned along with the remaining binary
+%%  - that if it is not a control character a parse error of the type
+%%    expected is thrown
+%%  - that if a non-binary is passed in that a badarg error is thrown
+crl_test_() ->
+    Codes = [127|lists:seq(0, 31)],
+    Passes = lists:map(fun(X) -> {<<X>>, X, <<>>} end, Codes),
+    Fails = lists:subtract(list_1(), lists:map(fun(X) -> <<X>> end, Codes)),
+    test_helper_(rfc2234, ctl, Passes, Fails, nonbinary_typeset()).
