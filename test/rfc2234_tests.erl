f8b9d8d488229cbd567a3cd6efd1bc1b8d0d564a
mikeyhc <mikeyhc@atmosia.net>
2015-04-13 16:22:17 +1200

    switched to macros for more useful test output

--- test/rfc2234_tests.erl
+++ test/rfc2234_tests.erl
@@ -6,6 +6,30 @@
 -module(rfc2234_tests).
 -compile([export_all]).
 -include_lib("eunit/include/eunit.hrl").
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Test Helper Macros %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% a helper test suite for the single character primitive functions
+-define(test_helper_(M, F, L1, L2, L3),
+    [ lists:map(fun({X, Y, Z}) -> ?_assertEqual({Y, Z}, M:F(X)) end, L1),
+      lists:map(fun(X) -> ?_assertThrow({parse_error, expected, _},
+                                        M:F(X)) end, L2),
+      lists:map(fun(X) -> ?_assertError({badarg, _}, M:F(X)) end, L3)]).
+
+%% generate a simple parser test
+-define(parser_test_(Func, Passes, Fails, Invalids),
+    ?test_helper_(rfc2234, Func, Passes, Fails, Invalids)).
+
+%% the most common parser test
+-define(single_binary_parser_test_(Func, Set),
+    Passes = int_list_to_pass_set(Set),
+    Fails = gen_rand_n(10, 1, lists:map(fun({X,_,_})-> X end, Passes)),
+    ?parser_test_(Func, Passes, Fails, nonbinary_typeset())).
+
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Test Helper Functions %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -27,14 +51,6 @@ gen_rand_n_(N, M, L) ->
 gen_rand_m(0) -> <<>>;
 gen_rand_m(M) -> <<(random:uniform(256) - 1),
                    (gen_rand_m(M - 1))/binary>>.
-
-
-%% a helper test suite for the single character primitive functions
-test_helper_(M, F, L1, L2, L3) ->
-    [ lists:map(fun({X, Y, Z}) -> ?_assertEqual({Y, Z}, M:F(X)) end, L1),
-      lists:map(fun(X) -> ?_assertThrow({parse_error, expected, _},
-                                        M:F(X)) end, L2),
-      lists:map(fun(X) -> ?_assertError({badarg, _}, M:F(X)) end, L3)].
 
 %% the list of the alphabet
 alphaset() -> lists:append(lists:seq(65, 90),   % A-Z
@@ -151,52 +167,41 @@ case_string_test_() ->
 %%    a parse error is thrown of type expected is thrown
 %%  - that if a non-binary is passed in a badarg error is thrown
 
-%% generate a simple parser test
-parser_test_(Func, Passes, Fails, Invalids) ->
-    test_helper_(rfc2234, Func, Passes, Fails, Invalids).
-
-%% the most common parser test
-single_binary_parser_test_(Func, Set) ->
-    Passes = int_list_to_pass_set(Set),
-    Fails = gen_rand_n(10, 1, lists:map(fun({X,_,_})-> X end, Passes)),
-    parser_test_(Func, Passes, Fails, nonbinary_typeset()).
-
-
 %% test alpha/1, accepts characters from the alphabet [65..90,97..122]
-alpha_test_() -> single_binary_parser_test_(alpha, alphaset()).
+alpha_test_() -> ?single_binary_parser_test_(alpha, alphaset()).
 
 %% test bit/1, accepts '0' and '1' [48, 49]
-bit_test_() -> single_binary_parser_test_(bit, [48, 49]).
+bit_test_() -> ?single_binary_parser_test_(bit, [48, 49]).
 
 %% test character/1, accepts a 7-bit character excluding NUL [1..127]
-character_test_() -> single_binary_parser_test_(character, lists:seq(1, 127)).
+character_test_() -> ?single_binary_parser_test_(character, lists:seq(1, 127)).
 
 %% test cr/1, accepts \r [13]
-cr_test_() -> single_binary_parser_test_(cr, [13]).
+cr_test_() -> ?single_binary_parser_test_(cr, [13]).
 
 %% test lf/1, accepts \n [10]
-lf_test_() -> single_binary_parser_test_(lf, [10]).
+lf_test_() -> ?single_binary_parser_test_(lf, [10]).
 
 %% test crlf/1, accepts "\r\n" [<<13,10>>]
 crlf_test_() ->
     Codes = [<<"\r\n">>],
     Passes = bin_list_to_pass_set(Codes),
-    parser_test_(crlf, Passes, gen_rand_n(10, 2, Codes), nonbinary_typeset()).
+    ?parser_test_(crlf, Passes, gen_rand_n(10, 2, Codes), nonbinary_typeset()).
 
 %% test crl/1, accepts US-ASCII control charcters [0..31,127]
-crl_test_() -> single_binary_parser_test_(ctl, lists:seq(0, 31)).
+crl_test_() -> ?single_binary_parser_test_(ctl, lists:seq(0, 31)).
 
 %% test dquote/1, accepts " [34]
-dquote_test_() -> single_binary_parser_test_(dquote, [34]).
+dquote_test_() -> ?single_binary_parser_test_(dquote, [34]).
 
 %% test hexdig/1, accepts a hexidecimal digit [48..57,65..70,97..102]
-hexdig_test_() -> single_binary_parser_test_(
+hexdig_test_() -> ?single_binary_parser_test_(
                     hexdig, lists:concat([ lists:seq(48, 57),
                                            lists:seq(65, 70),
                                            lists:seq(97, 102)])).
 
 %% test htab/1, accepts \t [9]
-htab_test_() -> single_binary_parser_test_(htab, [9]).
+htab_test_() -> ?single_binary_parser_test_(htab, [9]).
 
 %% test lwsp/1, this should accept any valid linear whitespace
 %% TODO: add some sensible fail tests
@@ -204,30 +209,30 @@ lwsp_test_() ->
     Passes = lists:map(fun(X) -> {X, X, <<>>} end,
                        [<<"  \r\n ">>, <<" \r\n\t">>, <<"\t\r\n ">>,
                         <<"\r\n ">>, <<" ">>, <<"\t">>]),
-    test_helper_(rfc2234, lwsp, Passes, [], nonbinary_typeset()).
+    ?test_helper_(rfc2234, lwsp, Passes, [], nonbinary_typeset()).
 
 %% test octet/1, accepts any octet [0..255]
 %% NOTE: type constraints mean there are no fail tests for this
 octet_test_() ->
-    parser_test_(octet, int_list_to_pass_set(lists:seq(0, 255)), [],
+    ?parser_test_(octet, int_list_to_pass_set(lists:seq(0, 255)), [],
                  nonbinary_typeset()).
 
 %% test sp/1, accepts ' ' [32]
-sp_test_() -> single_binary_parser_test_(sp, [32]).
+sp_test_() -> ?single_binary_parser_test_(sp, [32]).
 
 %% test vchar/1, accepts visible characters [33..126]
 vchar_test_() ->
     Codes = lists:seq(33, 126),
     Passes = lists:map(fun(X) -> {<<X>>, X, <<>>} end, Codes),
     Fails = gen_rand_n(10, 1, lists:map(fun(X) -> <<X>> end, Codes)),
-    test_helper_(rfc2234, vchar, Passes, Fails, nonbinary_typeset()).
+    ?test_helper_(rfc2234, vchar, Passes, Fails, nonbinary_typeset()).
 
 %% test wsp/1, accepts \t or ' ' [9, 32]
 wsp_test_() ->
     Codes = [9, 32],
     Passes = lists:map(fun(X) -> {<<X>>, X, <<>>} end, Codes),
     Fails = [ <<"a">>, <<"\n">>, <<"\r">>, <<"z">> ],
-    test_helper_(rfc2234, wsp, Passes, Fails, nonbinary_typeset()).
+    ?test_helper_(rfc2234, wsp, Passes, Fails, nonbinary_typeset()).
 
 %%%%%%%%%%%%%%%%%%%%%%%%
 %%% Useful Additions %%%
@@ -240,7 +245,7 @@ quoted_pair_test_() ->
                                     list_1())),
     Fails = lists:concat([gen_rand_n(8, 2, Passes),
                           [<<"\\\r">>, <<"\\\n">>]]),
-    test_helper_(rfc2234, quoted_pair, Passes, Fails, nonbinary_typeset()).
+    ?test_helper_(rfc2234, quoted_pair, Passes, Fails, nonbinary_typeset()).
 
 %% test quoted_string/1, accepts a quoted string
 % quoted_string_test_() ->
@@ -253,7 +258,7 @@ quoted_pair_test_() ->
 qtext_test_() ->
     Fails = [<<10>>, <<13>>, <<34>>],
     Passes = bin_list_to_pass_set(gen_rand_n(10, 1, Fails)),
-    parser_test_(qtext, Passes, Fails, nonbinary_typeset()).
+    ?parser_test_(qtext, Passes, Fails, nonbinary_typeset()).
 
 %% test qcont/1, it accepts either a 'qtext' or a 'quoted_pair'
 qcont_test_() ->
@@ -265,4 +270,4 @@ qcont_test_() ->
                               lists:map(fun(X) -> <<"\\", X/binary>> end,
                                         gen_rand_n(5, 1, [<<"\n">>, <<"\r">>]))
                             ])),
-    test_helper_(rfc2234, qcont, Passes, Fails, nonbinary_typeset()).
+    ?test_helper_(rfc2234, qcont, Passes, Fails, nonbinary_typeset()).
