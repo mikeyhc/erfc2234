c2c247b8458dbf596ef0e1d4fc6342985373b30b
mikeyhc <mikeyhc@atmosia.net>
2015-04-13 16:22:09 +1200

    added more tests, refactored some complex methods and fixed a few bugs

--- src/rfc2234.erl
+++ src/rfc2234.erl
@@ -11,7 +11,7 @@
 -module(rfc2234).
 %-compile(export_all).
 -export([% parser combinators
-         many/3, many1/3, option/4, either/6,
+         many/3, many1/3, option/4, either/6, both/6,
 
          % generic parsers
          case_char/2, case_string/2,
@@ -21,7 +21,7 @@
          hexdig/1, htab/1, lwsp/1, octet/1, sp/1, vchar/1, wsp/1,
 
          % helper parsers
-         qcont/1, qtext/1,
+         qcont/1, qtext/1, lwsp_/1,
 
          % useful additions
          quoted_pair/1, quoted_string/1]).
@@ -33,24 +33,27 @@
 %% match 0 or more times using M:F(A)
 many(M, F, A) ->
     try
-        {X, Y} = M:F(A),
+        {X, Y} = apply_many(M, F, A),
         {Acc, T} = many(M, F, Y),
-        {[X|Acc], T}
+        {<<X, Acc/binary>>, T}
     catch
-        {parse_error, expected, _} -> {[], A};
-        {badmatch, <<>>}          -> {[], A}
+        {parse_error, expected, _} -> {<<>>, A};
+        error:{badmatch, <<>>}     -> {<<>>, A}
     end.
 
 %% match 1 or more times using M:F(A)
 many1(M, F, A) ->
-    {X, Y} = M:F(A),
+    {X, Y} = apply_many(M, F, A),
     {Acc, T} = many(M, F, Y),
-    {[X|Acc], T}.
+    {<<X, Acc/binary>>, T}.
+
+apply_many(M, F, A) when is_list(A) -> apply(M, F, A);
+apply_many(M, F, A) -> M:F(A).
 
 %% tries to match M:F(A), but if that fails will return {Def, A}
 option(Def, M, F, A) ->
     try
-        M:F(A)
+        apply_many(M, F, A)
     catch
         {parse_error, expected, _} -> {Def, A}
     end.
@@ -60,14 +63,39 @@ either(M1, F1, M2, F2, A, Err) ->
     try
         M1:F1(A)
     catch
-        {parse_error, expected, _} ->
-            try
-                M2:F2(A)
-            catch
-                {parse_error, expected, _} ->
-                    throw({parse_error, expected, Err})
-            end
+        {parse_error, expected, _} -> either_(M2, F2, A, Err);
+        error:{badmatch, _} -> either_(M2, F2, A, Err)
     end.
+
+either_(M, F, A, Err) ->
+    try
+        M:F(A)
+    catch
+        {parse_error, expected, _} -> throw({parse_error, expected, Err});
+        error:{badmatch, _} -> throw({parse_error, expected, Err})
+    end.
+
+% tries to match both, if either fail it throws a parse error with the given
+% message
+both(M1, F1, M2, F2, A, Err) ->
+    try
+        {H1, T1} = apply_many(M1, F1, A),
+        {H2, T2} = apply_many(M2, F2, T1),
+        {bin_join(H1, H2), T2}
+    catch
+        {parse_error, expected, _} -> throw({parse_error, expected, Err});
+        error:{badmatch, _} -> throw({parse_error, expected, Err})
+    end.
+
+% joins characters and binarys
+bin_join(X, Y) when is_binary(X) andalso is_binary(Y) ->
+    <<X/binary, Y/binary>>;
+bin_join(X, Y) when is_integer(X) andalso is_binary(Y) ->
+    <<X, Y/binary>>;
+bin_join(X, Y) when is_binary(X) andalso is_integer(y) ->
+    <<X/binary, Y>>;
+bin_join(X, Y) -> <<X, Y>>.
+
 
 %% converts an uppercase character to its lowercase version, if
 %% it is not an uppercase character no change is performed
@@ -85,22 +113,24 @@ case_char(C, S) when is_binary(S) andalso is_integer(C) ->
         true -> {H,T};
         false -> throw({parse_error, expected, C})
     end;
-case_char(C, _) when not is_integer(C) -> throw({badarg, C});
+case_char(C, _) when not is_integer(C) -> error({badarg, C});
 case_char(_, S) -> error({badarg, S}).
 
 %% case insensitive string match
 case_string(<<>>, L) when is_binary(L) -> {<<>>, L};
+case_string(S, <<>>) when is_binary(S) ->
+    throw({parse_error, expected, S});
 case_string(S, L) when is_binary(L) andalso is_binary(S) ->
     <<SH, ST/binary>> = S,
     try
         {LH, LT} = case_char(SH, L),
         {RH, RT} = case_string(ST, LT),
-        {<<LH,RH>>, RT}
+        {<<LH,RH/binary>>, RT}
     catch
         {parse_error, expected, SH} ->
-            throw({parse_error, expected, ST});
+            throw({parse_error, expected, S});
         {parse_error, expected, NT} ->
-            throw({parse_error, expected, <<SH,NT>>})
+            throw({parse_error, expected, NT})
     end;
 case_string(S, _) when not is_binary(S) -> error({badarg, S});
 case_string(_, L) -> error({badarg, L}).
@@ -205,17 +235,17 @@ htab(X) -> error({badarg, X}).
 %% match "linear white-space". That is any number of consecutive 'wsp',
 %% optionally followed by a 'crlf' and (at least) one more 'wsp'.
 lwsp(X) when is_binary(X) ->
-    {H1, T1} = many1(rfc2234, wsp, X),
-    {H, T} = try
-        {H2, T2} = crlf(T1),
-        {H3, T3} = many1(rfc2234, wsp, T2),
-        {<<H2, H3>>, T3}
-    catch
-        {parse_error, expected, _} -> {[], T1}
-    end,
-    {NH, NT} = option(<<>>, rfc2234, lwsp, T),
-    {<<H1,H,NH>>, NT};
+    {H1, T1} = option(<<>>, rfc2234, many, [rfc2234, wsp, X]),
+    {H2, T2} = try
+                   both(rfc2234, crlf, rfc2234, lwsp_, T1,
+                        "crlf followed by whitespace")
+               catch
+                   {parse_error, expected, _} -> {<<>>, T1}
+               end,
+    {<<H1/binary, H2/binary>>, T2};
 lwsp(X) -> error({badarg, X}).
+
+lwsp_(X) -> many1(rfc2234, wsp, X).
 
 %% match any character
 octet(X) when is_binary(X) -> <<H, T/binary>> = X, {H, T};
@@ -249,10 +279,14 @@ wsp(X) -> error({badarg, X}).
 
 %% match a "quoted pair". Any characters (excluding CR and LF) may be quoted.
 quoted_pair(X)  when is_binary(X) ->
-    <<H, I, T/binary>> = X,
-    if H /= 92 orelse I == 10 orelse I == 13 ->
-           throw({parse_error, expected, "quoted pair"});
-       true -> {<<H,I>>, T}
+    try
+        <<H, I, T/binary>> = X,
+        if H /= 92 orelse I == 10 orelse I == 13 ->
+               throw({parse_error, expected, "quoted pair"});
+           true -> {<<H,I>>, T}
+        end
+    catch
+        error:{badmatch, _} -> throw({parse_error, expected, "quoted pair"})
     end;
 quoted_pair(X) -> error({badarg, X}).
 
@@ -262,18 +296,18 @@ quoted_string(X) when is_binary(X) ->
     {OQ, T1} = dquote(X),
     {C, T2} = many(rfc2234, qcont, T1),
     {CQ, T3} = dquote(T2),
-    {<<OQ,C,CQ>>, T3}.
+    {<<OQ,C/binary,CQ/binary>>, T3}.
 
 %% match the contents of a quoted string
 qcont(X) ->
-    either(rfc2234, qtext, rfc2234, quoted_pair, X,
+    either(rfc2234, quoted_pair, rfc2234, qtext, X,
            "quoted text or quoted pair").
 
 %% a binary which is valid in a quoted string
 qtext(X) when is_binary(X) ->
     <<H, T/binary>> = X,
-    if H == 10 orelse H == 13 orelse H == 32->
+    if H == 10 orelse H == 13 orelse H == 34 orelse H == 92 ->
            throw({parse_error, expected, "quoted text"});
-       true -> {H, T}
+       true -> {<<H>>, T}
     end;
 qtext(X) -> error({badarg, X}).
--- test/rfc2234_tests.erl
+++ test/rfc2234_tests.erl
@@ -4,11 +4,30 @@
 %% author: mikeyhc <mikeyhc@atmosia.net>
 
 -module(rfc2234_tests).
+-compile([export_all]).
 -include_lib("eunit/include/eunit.hrl").
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Test Helper Functions %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% genertes N random bytestrings of length M, filtered against L
+gen_rand_n(N, M, L) ->
+    <<A:32, B:32, C:32>> = crypto:rand_bytes(12),
+    random:seed(A, B, C),
+    gen_rand_n_(N, M, L).
+
+gen_rand_n_(0, _, _) -> [];
+gen_rand_n_(N, M, L) ->
+    H = gen_rand_m(M),
+    case lists:member(H,L) of
+        true  -> gen_rand_n_(N, M, L);
+        false -> [H|gen_rand_n_(N-1, M, L)]
+    end.
+
+gen_rand_m(0) -> <<>>;
+gen_rand_m(M) -> <<(random:uniform(256) - 1),
+                   (gen_rand_m(M - 1))/binary>>.
+
 
 %% a helper test suite for the single character primitive functions
 test_helper_(M, F, L1, L2, L3) ->
@@ -29,9 +48,17 @@ list_2() -> lists:concat(lists:map(fun(X) ->
                 lists:map(fun(Y) -> <<X,Y>> end, lists:seq(0, 255))
                 end, lists:seq(0,255))).
 
+%% the list of all common types which are not integers
+noninteger_typeset() -> [ [], <<>>,  a, "", 0.0, fun() -> {} end, {a, typle} ].
+
 %% the list of all common types which are not binary
-nonbinary_typeset() -> [ [], a, "", 0.0, fun() -> io:format("oh no~n") end,
-                         {a, tuple}].
+nonbinary_typeset() -> [ [], 0, a, "", 0.0, fun() -> {} end, {a, tuple}].
+
+% turns a set of integers into a pass set
+int_list_to_pass_set(L) -> lists:map(fun(X) -> {<<X>>, X, <<>>} end, L).
+
+% turns a set of binaries into a pass set
+bin_list_to_pass_set(L) -> lists:map(fun(X) -> {X, X, <<>>} end, L).
 
 %%%%%%%%%%%%%%%%%%%%%%%
 %%% Generic Parsers %%%
@@ -53,7 +80,37 @@ case_char_test_() ->
                   end,
                   lists:subtract(alphaset(), [X, X+32]))
                 end,
-                lists:seq(65, 90))
+                lists:seq(65, 90)),
+      lists:map(
+        fun(X) -> ?_assertError({badarg, _}, rfc2234:case_char(X, <<>>)) end,
+        noninteger_typeset()),
+      lists:map(
+        fun(X) -> ?_assertError({badarg, _}, rfc2234:case_char(<<>>, X)) end,
+        nonbinary_typeset())
+    ].
+
+% test case insensitive string matches
+case_string_test_() ->
+    [ lists:map(fun({Match, In}) ->
+                       ?_assertEqual({In, <<>>},
+                       rfc2234:case_string(Match, In))
+               end,
+               [ { <<"abc">>, <<"abc">> },
+                 { <<"XyZ">>, <<"xyz">> },
+                 { <<"ijk">>, <<"Ijk">> } ]),
+      lists:map(fun({Match, In}) ->
+                        ?_assertThrow({parse_error, expected, _},
+                                      rfc2234:case_string(Match, In))
+                end,
+                [ { <<"abc">>, <<"ab">> },
+                  { <<"ihj">>, <<"abc">> },
+                  { <<"abd">>, <<"abc">> } ]),
+      lists:map(
+        fun(X) -> ?_assertError({badarg, _}, rfc2234:case_string(X, <<>>)) end,
+        nonbinary_typeset()),
+      lists:map(
+        fun(X) -> ?_assertError({badarg, _}, rfc2234:case_string(<<>>, X)) end,
+        nonbinary_typeset())
     ].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
@@ -68,107 +125,82 @@ case_char_test_() ->
 %%    a parse error is thrown of type expected is thrown
 %%  - that if a non-binary is passed in a badarg error is thrown
 
+%% generate a simple parser test
+parser_test_(Func, Passes, Fails, Invalids) ->
+    test_helper_(rfc2234, Func, Passes, Fails, Invalids).
+
+%% the most common parser test
+single_binary_parser_test_(Func, Set) ->
+    Passes = int_list_to_pass_set(Set),
+    Fails = gen_rand_n(10, 1, lists:map(fun({X,_,_})-> X end, Passes)),
+    parser_test_(Func, Passes, Fails, nonbinary_typeset()).
+
+
 %% test alpha/1, accepts characters from the alphabet [65..90,97..122]
-alpha_test_() ->
-    Passes = lists:map(fun(X) -> {<<X,"bc">>, X, <<"bc">>} end, alphaset()),
-    Fails = lists:map(fun(X) -> <<X, "12">> end,
-                      lists:subtract(lists:seq(0, 255), alphaset())),
-    test_helper_(rfc2234, alpha, Passes, Fails, nonbinary_typeset()).
+alpha_test_() -> single_binary_parser_test_(alpha, alphaset()).
 
 %% test bit/1, accepts '0' and '1' [48, 49]
-bit_test_() ->
-    Passes = lists:map(fun(X) -> {<<X,"01">>, X, <<"01">>} end, [48, 49]),
-    Fails = lists:map(fun(X) -> <<X, "bc">> end,
-                       lists:subtract(lists:seq(0, 255), [48, 49])),
-    test_helper_(rfc2234, bit, Passes, Fails, nonbinary_typeset()).
+bit_test_() -> single_binary_parser_test_(bit, [48, 49]).
 
 %% test character/1, accepts a 7-bit character excluding NUL [1..127]
-character_test_() ->
-    Passes = lists:map(fun(X) -> {<<X,"bc">>, X, <<"bc">>} end,
-                       lists:seq(1, 127)),
-    Fails = lists:map(fun(X) -> <<X>> end, [0|lists:seq(128, 256)]),
-    test_helper_(rfc2234, character, Passes, Fails, nonbinary_typeset()).
+character_test_() -> single_binary_parser_test_(character, lists:seq(1, 127)).
 
 %% test cr/1, accepts \r [13]
-cr_test_() ->
-    Fails = lists:map(fun(X) -> <<X>> end,
-                      lists:delete(13, lists:seq(0, 256))),
-    test_helper_(rfc2234, cr, [{<<"\rab">>, 13, <<"ab">>}], Fails,
-                 nonbinary_typeset()).
+cr_test_() -> single_binary_parser_test_(cr, [13]).
 
 %% test lf/1, accepts \n [10]
-lf_test_() ->
-    Fails = lists:map(fun(X) -> <<X>> end,
-                      lists:delete(10, lists:seq(0, 256))),
-    test_helper_(rfc2234, lf, [{<<"\nab">>, 10, <<"ab">>}], Fails,
-                 nonbinary_typeset()).
+lf_test_() -> single_binary_parser_test_(lf, [10]).
 
 %% test crlf/1, accepts "\r\n" [<<13,10>>]
 crlf_test_() ->
-    Fails = lists:delete(<<"\r\n">>, list_2()),
-    test_helper_(rfc2234, crlf, [{<<"\r\n-">>, <<"\r\n">>, <<"-">>}],
-                 Fails, nonbinary_typeset()).
+    Codes = [<<"\r\n">>],
+    Passes = bin_list_to_pass_set(Codes),
+    parser_test_(crlf, Passes, gen_rand_n(10, 2, Codes), nonbinary_typeset()).
 
 %% test crl/1, accepts US-ASCII control charcters [0..31,127]
-crl_test_() ->
-    Codes = [127|lists:seq(0, 31)],
-    Passes = lists:map(fun(X) -> {<<X>>, X, <<>>} end, Codes),
-    Fails = lists:subtract(list_1(), lists:map(fun(X) -> <<X>> end, Codes)),
-    test_helper_(rfc2234, ctl, Passes, Fails, nonbinary_typeset()).
+crl_test_() -> single_binary_parser_test_(ctl, lists:seq(0, 31)).
 
 %% test dquote/1, accepts " [34]
-dquote_test_() ->
-    Passes = [{<<"\"">>, 34, <<>>}],
-    Fails = lists:delete(<<"\"">>, list_1()),
-    test_helper_(rfc2234, dquote, Passes, Fails, nonbinary_typeset()).
+dquote_test_() -> single_binary_parser_test_(dquote, [34]).
 
 %% test hexdig/1, accepts a hexidecimal digit [48..57,65..70,97..102]
-hexdig_test_() ->
-    Codes = lists:concat([ lists:seq(48, 57),
-                           lists:seq(65, 70),
-                           lists:seq(97, 102)]),
-    Passes = lists:map(fun(X) -> {<<X>>, X, <<>>} end, Codes),
-    Fails  = lists:subtract(list_1(), lists:map(fun(X) -> <<X>> end, Codes)),
-    test_helper_(rfc2234, hexdig, Passes, Fails, nonbinary_typeset()).
+hexdig_test_() -> single_binary_parser_test_(
+                    hexdig, lists:concat([ lists:seq(48, 57),
+                                           lists:seq(65, 70),
+                                           lists:seq(97, 102)])).
 
 %% test htab/1, accepts \t [9]
-htab_test_() ->
-    Passes = [{<<"\t">>, 9, <<>>}],
-    Fails = lists:delete(<<"\t">>, list_1()),
-    test_helper_(rfc2234, htab, Passes, Fails, nonbinary_typeset()).
+htab_test_() -> single_binary_parser_test_(htab, [9]).
 
 %% test lwsp/1, this should accept any valid linear whitespace
-%lwsp_test_() ->
-%    Passes = lists:map(fun(X) -> {X, X, <<>>} end,
-%                       [<<"  \r\n ">>, <<" \r\n\t">>, <<"\t\r\n">>,
-%                        <<"\r\n ">>, <<" ">>, <<"\t">>]),
-%    Fails = [<<"a">>, <<"\r\n">>],
-%    test_helper_(rfc2234, lwsp, Passes, Fails, nonbinary_typeset()).
+%% TODO: add some sensible fail tests
+lwsp_test_() ->
+    Passes = lists:map(fun(X) -> {X, X, <<>>} end,
+                       [<<"  \r\n ">>, <<" \r\n\t">>, <<"\t\r\n ">>,
+                        <<"\r\n ">>, <<" ">>, <<"\t">>]),
+    test_helper_(rfc2234, lwsp, Passes, [], nonbinary_typeset()).
 
 %% test octet/1, accepts any octet [0..255]
 %% NOTE: type constraints mean there are no fail tests for this
 octet_test_() ->
-    Passes = lists:map(fun(X) -> {<<X>>, X, <<>>} end, lists:seq(0, 255)),
-    test_helper_(rfc2234, octet, Passes, [], nonbinary_typeset()).
+    parser_test_(octet, int_list_to_pass_set(lists:seq(0, 255)), [],
+                 nonbinary_typeset()).
 
 %% test sp/1, accepts ' ' [32]
-sp_test_() ->
-    Passes = [{<<" ">>, 32, <<>>}],
-    Fails = lists:delete(<<" ">>, list_1()),
-    test_helper_(rfc2234, sp, Passes, Fails, nonbinary_typeset()).
+sp_test_() -> single_binary_parser_test_(sp, [32]).
 
 %% test vchar/1, accepts visible characters [33..126]
 vchar_test_() ->
     Codes = lists:seq(33, 126),
     Passes = lists:map(fun(X) -> {<<X>>, X, <<>>} end, Codes),
-    Fails = lists:subtract(list_1(), lists:map(fun(X) -> <<X>> end, Codes)),
+    Fails = gen_rand_n(10, 1, lists:map(fun(X) -> <<X>> end, Codes)),
     test_helper_(rfc2234, vchar, Passes, Fails, nonbinary_typeset()).
 
 %% test wsp/1, accepts \t or ' ' [9, 32]
 wsp_test_() ->
     Codes = [9, 32],
     Passes = lists:map(fun(X) -> {<<X>>, X, <<>>} end, Codes),
-    Fails = lists:subtract(list_1(), lists:map(fun(X) -> <<X>> end, Codes)),
+    Fails = [ <<"a">>, <<"\n">>, <<"\r">>, <<"z">> ],
     test_helper_(rfc2234, wsp, Passes, Fails, nonbinary_typeset()).
 
 %%%%%%%%%%%%%%%%%%%%%%%%
@@ -180,8 +212,24 @@ quoted_pair_test_() ->
     Passes = lists:map(fun(<<Y>>) -> {<<"\\",Y>>, <<"\\",Y>>, <<>>} end,
                        lists:filter(fun(<<X>>) -> X /= 10 andalso X /= 13 end,
                                     list_1())),
-    Fails = lists:subtract(list_2(),
-                           lists:map(fun({X, _, _}) -> X end, Passes)),
+    Fails = lists:concat([gen_rand_n(8, 2, Passes),
+                          [<<"\\\r">>, <<"\\\n">>]]),
     test_helper_(rfc2234, quoted_pair, Passes, Fails, nonbinary_typeset()).
 
-%% test qtex/1, accepts binary strings not containing \r or \n, returns on  "
+%% test qtext/1, accepts any character that is not \r \n or "
+qtext_test_() ->
+    Fails = [<<10>>, <<13>>, <<34>>],
+    Passes = bin_list_to_pass_set(gen_rand_n(10, 1, Fails)),
+    parser_test_(qtext, Passes, Fails, nonbinary_typeset()).
+
+%% test qcont/1, it accepts either a 'qtext' or a 'quoted_pair'
+qcont_test_() ->
+    SingleFails = [<<"\r">>, <<"\n">>, <<"\"">> ],
+    DoubleFails = [<<"\\\n">>, <<"\\\r">>],
+    Fails = lists:concat([SingleFails, DoubleFails]),
+    Passes = bin_list_to_pass_set(
+               lists:concat([ gen_rand_n(5, 1, SingleFails),
+                              lists:map(fun(X) -> <<"\\", X/binary>> end,
+                                        gen_rand_n(5, 1, [<<"\n">>, <<"\r">>]))
+                            ])),
+    test_helper_(rfc2234, qcont, Passes, Fails, nonbinary_typeset()).
